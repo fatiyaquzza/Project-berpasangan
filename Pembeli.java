a64352d9f00c5977a359974243921b170c74024f
fatiyaquzza <fatiya_qu@mhs.unsyiah.ac.id>
2022-11-09 01:57:59 +0700

    Menambahkan class Pembeli disini terdapat 9 method, dimana 2 adalah constructor, lalu ada 3 method accessor dan 4 method mutator Terdapat 3 variabel pada class Pembeli ini.

---
+++ Pembeli.java
@@ -0,0 +1,45 @@
+public class Pembeli {
+    
+    private String nama;
+    private double totalBayar;
+    private int totalBarang;
+
+    public Pembeli(String nama, double totalBayar, int totalBarang) {
+        this.nama = nama;
+        this.totalBayar = totalBayar;
+        this.totalBarang = totalBarang;
+    }
+
+    public Pembeli(){
+        
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public double getTotalBayar() {
+        return totalBayar;
+    }
+
+    public void setTotalBayar(double totalBayar) {
+        this.totalBayar = totalBayar;
+    }
+
+    public int getTotalBarang() {
+        return totalBarang;
+    }
+
+    public void setTotalBarang(int totalBarang) {
+        this.totalBarang = totalBarang;
+    }
+
+    public void invoice(){
+        System.out.println("Total Harga: Rp." + getTotalBayar());
+        System.out.println("Jumlah Barang: " + getTotalBarang());
+    }
+}
